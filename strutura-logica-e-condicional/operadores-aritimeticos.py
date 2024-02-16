104bd60a7f46c85d2c35f7cc1f2eb44f3c6d9bd4
alissonrhuant3 <121673922+alissonrhuant3@users.noreply.github.com>
2024-02-16 11:59:04 -0300

    Estrutura logica e condicional e inicio do projeto com tkinter

--- strutura-logica-e-condicional/operadores-aritimeticos.py
+++ strutura-logica-e-condicional/operadores-aritimeticos.py
@@ -2,3 +2,22 @@
 # - Subtracao
 # / Divisao
 # *  Multiplicacao
+
+a = 5
+b = 4
+c = 2 
+d = 50
+
+print(a + d)
+print(b - a)
+print(d / b)
+print(a * d)
+
+print()
+
+# % Modulo
+# ** Expoenciacao
+# // Divisao inteira
+print(d % 7)
+print(b ** c)
+print(d // b)
