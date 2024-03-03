00d3e68db1deac90d29febde1fb52a1efdef134e
NycolasES6 <nycolasonrails@gmail.com>
2024-03-02 22:39:51 -0300

    Update dicionario.py

--- dicionario/dicionario.py
+++ dicionario/dicionario.py
@@ -11,14 +11,20 @@ tupla = ("valor1", "valor2", "valor3", "valor2")
 print("\n--- --- ---")
 dicio = {"chave1" : "Nycolas", "chave2" : 25, "chave3" : "Gabriel", "chave4" : True}
 
+print("\n--- --- ---")
 print(dir(dicio))
+
+print("\n--- --- ---")
 print(f"\n{dicio}")
 
+print("\n--- --- ---")
 print(f"\n{dicio['chave4']}")
 
+print("\n--- --- ---")
 print(f"\n{len(dicio)}")
 
+print("\n--- --- ---")
 print(f"\n{dicio.keys()}")
 
+print("\n--- --- ---")
 print(f"\n{dicio.values()}")
-print("\n")
