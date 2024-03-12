20c2398b6530b8f084ab3e1f66742e40f6edf807
Nycolas Senai <nycolasonrails@gmail.com>
2024-03-12 13:02:02 -0300

    Final of jogo da velha

--- projetos/pedra_papel_tesoura.py
+++ projetos/pedra_papel_tesoura.py
@@ -1,31 +1,83 @@
 from random import choice
 
-def escolha_maquina() :
+escolhas = ["PEDRA", "PAPEL", "TESOURA"]
+escolhas_perd = ["TESOURA", "PEDRA", "PAPEL"]
+
+
+pontos_jogador = 0
+pontos_maquina = 0
+
+
+"""Escolha da maquina"""
+
+
+def escolha_maquina():
     return choice(["PEDRA", "PAPEL", "TESOURA", "PAPEL", "PEDRA", "TESOURA"])
 
-def escolha_jogador() :
-    escolha = input("Qual a sua escolha ? \n PEDRA \n PAPEL \n TESOURA \n ? ")
+
+"""Escolha do jagador"""
+
+
+def escolha_jogador():
+    escolha = input("\n \n Qual a sua escolha ? \n - PEDRA \n - PAPEL \n - TESOURA \n ? ")
     escolha = escolha.upper()
 
-    while escolha not in ["PEDRA", "PAPEL", "TESOURA"]:
+    while escolha not in escolhas:
         print("Escolha inválida !! \n")
-        escolha = input("Qual a sua escolha ? \n PEDRA \n PAPEL \n TESOURA \n ? ")
+        escolha = input("\n\n Qual a sua escolha ? \n - PEDRA \n - PAPEL \n - TESOURA \n ? ")
         escolha = escolha.upper()
 
     return escolha
 
-quer_jogar = input("Quer jogar comigo ? \n [ s ] - SIM \n [ n ] NAO \n ? ").upper()
 
-if quer_jogar in ["S", "SIM"] :
-    quer_jogar = True
-else:
-    quer_jogar = False
-    print("Ficou com medo ?")
+"""Confere o resultado da partida"""
 
-while quer_jogar :
+
+def confere_resultado(escolha_do_jogador, escolha_da_maquina):
+    for key, esc in enumerate(escolhas):
+        if escolha_do_jogador == esc and escolha_da_maquina == escolhas_perd[key]:
+            global pontos_jogador
+            pontos_jogador += 1
+            return "O jogador venceu"
+            break
+        elif escolha_do_jogador == escolha_da_maquina:
+            return "Empate"
+            break
+        else:
+            global pontos_maquina
+            pontos_maquina += 1
+            return "A maquina venceu"
+            break
+
+
+"""Retorna o placar da partida"""
+
+
+def mostra_placar():
+    return f"\nPlacar - - - - - \n Jogador - {pontos_jogador} \n Máquina - {pontos_maquina}\n\n"
+
+
+quer_jogar = True
+
+while quer_jogar:
+    quer_jogar = input("Quer jogar comigo ? \n [ s ] - SIM \n [ n ] - NAO \n ? ").upper()
+
+    if quer_jogar in ["S", "SIM"]:
+        quer_jogar = True
+    else:
+        quer_jogar = False
+        print(mostra_placar())
+        print("Ficou com medo ?")
+        break
+
     print("A partida começou -----------------")
     escolha_do_jogador = escolha_jogador()
     escolha_da_maquina = escolha_maquina()
 
-    print(f,"Escolha do jogador foi {escolha_do_jogador}")
-    print(escolha_da_maquina)
+    print(f"A escolha do jogador foi {escolha_do_jogador}")
+    print(f"A escolha da maquina foi {escolha_da_maquina}")
+
+    print(f"\n{escolha_do_jogador} X {escolha_da_maquina}")
+    print(f"\n{confere_resultado(escolha_do_jogador, escolha_da_maquina)}")
+
+    print(mostra_placar())
